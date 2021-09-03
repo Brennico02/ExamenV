110e0eb7ec543e0840d8974696577b51fbe6c765
Brennico02 <88550613+Brennico02@users.noreply.github.com>
2021-09-03 10:20:50 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,19 @@
-input.onButtonPressed(Button.B, function () {
-    index += 1
-    Volumen += 51
-    music.setVolume(Volumen)
-    for (let index = 0; index <= contador; index++) {
-        led.plot(x, y - index)
+let numero = 0
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < numero; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.clearScreen()
+        basic.pause(1000)
     }
-    contador += 1
 })
-let index = 0
-let contador = 0
-let y = 0
-let x = 0
-let Volumen = 0
-music.setVolume(51)
-Volumen = 51
-x = 0
-y = 4
-contador = 1
-led.plot(x, y)
-basic.forever(function () {
-    music.playMelody("G F G A - F E D ", 120)
+input.onGesture(Gesture.Shake, function () {
+    numero = randint(1, 6)
+    basic.showNumber(numero)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let indice = 0; indice <= numero; indice++) {
+        for (let index = 0; index < numero; index++) {
+        	
+        }
+    }
 })
